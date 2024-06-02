d217750c0269f65fb8dcb2b3384f84f672ff57d3
Louis Pocheron <louis.pocheron@davikingcode.com>
2024-06-02 16:23:54 +0200

    feat: font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ import { useRouter } from 'next/navigation'
 export default function Home() {
   const [isLastWord, setIsLastWord] = useState<boolean>(false);
   const router = useRouter();
-  useEffect(() => {
-    if (!isLastWord) return;
-    const timeOut = setTimeout(() => {
-      router.push("/a-propos");
-    }, 5000)
+  // useEffect(() => {
+  //   if (!isLastWord) return;
+  //   const timeOut = setTimeout(() => {
+  //     router.push("/a-propos");
+  //   }, 5000)
 
-    return () => clearTimeout(timeOut)
-  }, [isLastWord]);
+  //   return () => clearTimeout(timeOut)
+  // }, [isLastWord]);
 
   return (
     <div className={style.container}>
